77141476fea2573ea33e06751274cc8364c9a167
G4CiO <86733145+G4CiO@users.noreply.github.com>
2024-09-28 02:54:09 +0700

    ik node finish

--- src/fun4_description/scripts/inverse_pose_kinematics.py
+++ src/fun4_description/scripts/inverse_pose_kinematics.py
@@ -23,6 +23,7 @@ class InversePoseKinematics(Node):
         self.joint_state_pub = self.create_publisher(JointState, '/joint_states', 10)
         # Sub
         # self.target_sub = self.create_subscription(PoseStamped, '/target', self.callback_target, 10)
+        self.joint_state_sub = self.create_subscription(JointState, '/joint_states', self.joint_state_callback, 10)
 
         # Service
         # Client Server (ส่ง request ออกไป,เรียกใช้ server)
@@ -31,10 +32,16 @@ class InversePoseKinematics(Node):
         # Service Server (รับ mode เข้ามา)
         self.take_mode = self.create_service(ChangeMode, '/mode_pose', self.callback_user)
 
+        # Variables to track joint states
+        self.current_joint_positions = [0.0, 0.0, 0.0]
+
         # Innitial
         self.mode = 0
         self.x ,self.y, self.z = 0.0, 0.0, 0.0
         self.finish = None
+        self.r_min = 0.03
+        self.r_max = 0.530
+        self.answer = []
 
     # def call_mode(self, q):
     #     while not self.mode_client.wait_for_service(1.0):
@@ -44,6 +51,10 @@ class InversePoseKinematics(Node):
 
     #     request.mode = q
     #     self.mode_client.call_async(request)
+    def joint_state_callback(self, msg):
+        """Callback to track current joint positions"""
+        self.current_joint_positions = list(msg.position)
+        self.get_logger().info(f'Current joint positions: {self.current_joint_positions}')
 
 
     def custom_ikine(self, robot, T_desired, initial_guess):
@@ -81,25 +92,44 @@ class InversePoseKinematics(Node):
         return response
 
     def inverse_kinematic(self, x, y, z, mode):
-        if mode == 1:
+        distance_squared = x**2 + y**2 + (z-0.2)**2
+        if mode == 1 and self.r_min**2 <= distance_squared <= self.r_max**2:
+            # robot = rtb.DHRobot(
+            #     [
+            #         rtb.RevoluteMDH(d=0.2), # Joint1
+            #         rtb.RevoluteMDH(alpha=-pi/2,d=-0.12), # Joint2
+            #         rtb.RevoluteMDH(alpha=pi/2,d=0.25), # JointN
+            #         rtb.RevoluteMDH(alpha=-pi/2,d=0.1), # Joint3
+            #         rtb.RevoluteMDH(alpha=pi/2,d=0.28), # e
+            #     ],
+            #     name = "RRR_Robot" # เขียนชื่อหุ่นยนต์ 
+            # )
+
             robot = rtb.DHRobot(
                 [
-                    rtb.RevoluteMDH(d=0.2), # Joint1
-                    rtb.RevoluteMDH(alpha=-pi/2,d=-0.12), # Joint2
-                    rtb.RevoluteMDH(alpha=pi/2,d=0.25), # JointN
-                    rtb.RevoluteMDH(alpha=-pi/2,d=0.1), # Joint3
-                    rtb.RevoluteMDH(alpha=pi/2,d=0.28), # e
-                ],
-                name = "RRR_Robot" # เขียนชื่อหุ่นยนต์ 
+                    rtb.RevoluteMDH(alpha = 0.0,a = 0.0,d = 0.2,offset = 0.0),
+                    rtb.RevoluteMDH(alpha = pi/2,a = 0.0,d = 0.02,offset = 0.0),
+                    rtb.RevoluteMDH(alpha = 0,a = 0.25,d = 0.0,offset = 0.0),
+
+                ],tool = SE3.Tx(0.28),
+                name = "RRR_Robot"
             )
 
             # Desired end effector pose
             T_Position = SE3(x, y, z)
+            # T_Orentation = SE3.RPY([radians(30),radians(45),radians(60)], order='zyx')
+            
 
             # Use the custom IK function
-            initial_guess = [0, 0, 0 ,0 ,0]
-                
-            q_sol_ik_LM = self.custom_ikine(robot, T_Position, initial_guess)
+            # initial_guess = [0, 0, 0 ,0 ,0]
+            temp = [self.current_joint_positions[0],self.current_joint_positions[1],0,self.current_joint_positions[2],0]
+            initial_guess = temp
+            # q_sol_ik_LM = self.custom_ikine(robot, T_Position, initial_guess)
+            # Use default IK
+            q_sol_ik_LM, *_ = robot.ikine_LM(T_Position,mask=[1,1,1,0,0,0],joint_limits=False,q0=[0,0,0])
+
+            Te = robot.fkine(q_sol_ik_LM)
+            self.get_logger().info(f'{Te}')
 
             joint_msg = JointState()
             joint_msg.name = ['joint_1', 'joint_2','joint_3']
@@ -107,22 +137,21 @@ class InversePoseKinematics(Node):
             # Initialize the 'position' list with 3 elements
             joint_msg.position = [0.0] * 3  # Ensure the list has enough elements
             joint_msg.header.stamp = self.get_clock().now().to_msg()
-            joint_msg.position[0] = q_sol_ik_LM[0]
-            joint_msg.position[1] = q_sol_ik_LM[1]
-            joint_msg.position[2] = q_sol_ik_LM[3]
+            joint_msg.position = [q_sol_ik_LM[0],q_sol_ik_LM[1],q_sol_ik_LM[2]]
 
             self.joint_state_pub.publish(joint_msg)
             self.finish = True
 
-            answer = [q_sol_ik_LM[0], q_sol_ik_LM[1], q_sol_ik_LM[3]]
+            self.answer = [q_sol_ik_LM[0], q_sol_ik_LM[1], q_sol_ik_LM[2]]
 
             self.get_logger().info('call finish')
 
-            return answer
+
+            return self.answer
         else:
             self.finish = False
-            self.q_sol = [0.0 ,0.0 ,0.0]
             self.get_logger().info('call not finish')
+            return self.answer
     
 
 
